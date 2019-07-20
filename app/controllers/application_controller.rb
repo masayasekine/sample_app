ba7936f4606f721b26a4118dcf8e7e3dd7dc2dd0
masayasekine <negirider250@gmail.com>
2019-07-20 09:33:21 +0000

    Finish Static Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  def hello
-    render html: "hello sample_app!"
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  #root 'static_pages#home'
+  
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -7,5 +10,5 @@ Rails.application.routes.draw do
   
   get 'static_pages/contact'
 
-  root 'application#hello'
+  
 end
