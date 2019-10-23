7b13cb6655be167c01c5648d22dd4184dad49668
masayasekine <negirider250@gmail.com>
2019-10-23 14:27:50 +0000

    add user_edit_form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,19 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    # TODO success for update
+    else
+      render 'edit'
+    end
+  end
 
   private
 
